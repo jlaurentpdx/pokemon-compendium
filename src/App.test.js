13171bf05b7adc621021fba4883361c7f478ba8c
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-13 18:09:55 -0800

    added snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Pokédex Compendium/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a load state, then a filtered list of Pokemon', async () => {
+  const { container } = render(<App />);
+
+  screen.getByText(/Pokédex Compendium/i);
+  await screen.findByText('abomasnow');
+
+  expect(container).toMatchSnapshot();
 });
